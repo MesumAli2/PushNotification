6187be74ea91f719254997d1502567679eae984b
MesumAli2 <smesumali20@gmail.com>
2022-04-03 22:22:32 +0400

    Initial commit

---
+++ app/src/main/java/com/mesum/pushnotification/FirebaseTokenService.kt
@@ -0,0 +1,7 @@
+package com.mesum.pushnotification
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class FirebaseTokenService : FirebaseMessagingService() {
+
+}
---
+++ app/src/main/java/com/mesum/pushnotification/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.mesum.pushnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.installations.remote.FirebaseInstallationServiceClient
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+}
